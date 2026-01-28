08e3bbef79600b32bdbe4d39632b54974efd4d27
liangym@dehuinet.com <liangym@dehuinet.com>
2026-01-28 15:41:33 +0800

    @/ => ./

---
+++ theme/themeConfig.ts
@@ -0,0 +1,34 @@
+import type { ThemeConfig } from 'antd';
+
+const mytheme: ThemeConfig = {
+  token: {
+    fontSize: 18,
+    colorPrimary: '#3c7963',
+    colorBgContainer: '#3c7963',
+    colorBorder: '#ffffffC8',
+  },
+  components: {
+    Menu: {
+      darkPopupBg: '#458c71',
+      darkItemColor: '#ffffffC8',
+    },
+    Table: {
+      colorBgContainer: '#ffffffC8',
+      // footerBg: '#ffffffC8',
+    },
+    Pagination: {
+      // itemActiveBg: '#ffffffC8',
+      // itemBg: '#ffffffC8',
+      // itemInputBg: '#ffffffC8',
+      colorBgContainer: '#ffffffC8',
+    },
+    Select: {
+      colorBgContainer: '#ffffffC8',
+    },
+    Breadcrumb: {
+      fontSize: 20,
+    },
+  }
+};
+
+export default mytheme;
