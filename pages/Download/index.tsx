11f242caaf3e07d9dbc7342db159393669752d3a
liangym@dehuinet.com <liangym@dehuinet.com>
2026-02-01 15:41:37 +0800

    基因组

--- pages/Download/index.tsx
+++ pages/Download/index.tsx
@@ -9,10 +9,66 @@ interface DataType {
   Genome?: string;
   GFF?: string;
   CDS?: string;
+  mRNA?: string;
   Protein?: string;
 }
 const { Content } = Layout;
 const columns: TableProps<DataType>['columns'] = [
+  {
+    title: 'Species',
+    dataIndex: 'Species',
+    key: 'Species',
+  },
+  {
+    title: 'Genome',
+    dataIndex: 'Genome',
+    key: 'Genome',
+    render: (text) => <a>FASTA</a>,
+  },
+  {
+    title: 'GFF',
+    dataIndex: 'GFF',
+    key: 'GFF',
+    render: (text) => <a>GFF</a>,
+  },
+  {
+    title: 'CDS',
+    dataIndex: 'CDS',
+    key: 'CDS',
+    render: (text) => <a>FASTA</a>,
+  },
+  {
+    title: 'mRNA',
+    dataIndex: 'mRNA',
+    key: 'mRNA',
+    render: (text) => <a>FASTA</a>,
+  },
+  {
+    title: 'Protein',
+    dataIndex: 'Protein',
+    key: 'Protein',
+    render: (text) => <a>FASTA</a>,
+  },
+];
+const dataSource: DataType[] = [
+  {
+    Species: '河岸葡萄',
+    key: 'Riverbank',
+  },
+  {
+    Species: '美洲葡萄',
+    key: 'America',
+  },
+  {
+    Species: '沙地葡萄',
+    key: 'Sandy',
+  },
+  {
+    Species: '圆叶葡萄',
+    key: 'Round',
+  }
+]
+const columns2: TableProps<DataType>['columns'] = [
   {
     title: 'Species',
     dataIndex: 'Species',
@@ -43,31 +99,13 @@ const columns: TableProps<DataType>['columns'] = [
     render: (text) => <a>FASTA</a>,
   },
 ];
-const dataSource: DataType[] = [
-  {
-    Species: '河岸葡萄',
-    key: 'Riverbank',
-  },
-  {
-    Species: '美洲葡萄',
-    key: 'America',
-  },
-  {
-    Species: '沙地葡萄',
-    key: 'Sandy',
-  },
-  {
-    Species: '圆叶葡萄',
-    key: 'Round',
-  }
-]
 const dataSource2: DataType[] = [
   {
-    Species: '2022年葡萄基因组12X.51',
+    Species: '‘黑比诺’葡萄12X',
     key: '2022',
   },
   {
-    Species: '2023年葡萄基因组PN_T2T',
+    Species: '‘黑比诺’葡萄PN_T2T',
     key: '2023',
   },
 ]
@@ -95,7 +133,7 @@ const Home = () =>
       </div>
       <Table
         // title={() => '基因组序列'}
-        columns={columns}
+        columns={columns2}
         dataSource={dataSource2}
         scroll={{ x: 'max-content' }}
         bordered
